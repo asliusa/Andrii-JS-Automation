98ae41677ffa128294e368ca7587e22213d9f288
asliusa <andrii.sliusar@teadochealth.com>
2023-12-24 15:24:16 +0200

    delete example tests

--- tests/store.spec.js
+++
@@ -1,9 +0,0 @@
-// @ts-check
-const { test, expect } = require('@playwright/test');
-
-test('show store home page', async ({ page }) => {
-    await page.goto('http://localhost:3000/');
-  
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Depot/);
-  });
